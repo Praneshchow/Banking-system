c82e7838b7c7949d10ace410ceee41af4a03c49c
Pranesh98 <praneshchy23@gmail.com>
2023-09-23 21:56:11 +0600

    first login page setup

---
+++ login.js
@@ -0,0 +1,21 @@
+
+// step 1: add click event handler with the submit button. 
+document.getElementById('btn-submit').addEventListener('click', function(){
+    // step 2: get the email address inside the email input field. 
+    // Always remember to use .value for taking input value. 
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    
+    // step 3: take the password from input field. 
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    
+    // step 4: verify the email and password. 
+    if (email === 'pranesh@gamil.com' && password === 'secret'){
+        console.log('valid user');
+    }
+    else{
+        console.log('invalid user');
+    }
+});
+
