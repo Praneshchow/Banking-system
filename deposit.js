ac1aeecd1c92626a6ad39bb41524b893429c162e
Pranesh98 <praneshchy23@gmail.com>
2023-09-24 17:45:00 +0600

    withdraw part complete

--- deposit.js
+++ deposit.js
@@ -1,31 +1,41 @@
-
+/*
+----------- Deposit Part -------------
 // step 1: add event listener to the deposit button. 
+// step 2: get the deposit amount from the deposit input field.
+// step 3: get the current deposit total
+// step 4: add numbers of the total deposit. (Must convert variable value string to float)
+// step 5: Get the balance of the current. 
+// step 6: get balance current total. 
+// step 7: deposit field will be clear.
+*/
+
+
+// Step 1: (First click the button. Than it works)
 document.getElementById('btn-deposit').addEventListener('click', function(){
-    // step 2: get the deposit amount from the deposit input field. 
+    // Step 2. (Deposit Part)
     // for input field use .value to the deposit input value. 
     const depositField = document.getElementById('deposit-field');       // depositField is deposit input. 
     const newDepositAmount = depositField.value;
     
-    // step 3: get the current deposit total 
+     
     // use innerText for taking the text 
     const depositTotalElement = document.getElementById('deposit-total');    // deposit output. 
-    
     const previousDepositTotal = depositTotalElement.innerText;
 
-    // step 4: add numbers of the total deposit. (Must convert variable value string to float)
+    
     const currentDepositTotal = parseFloat(previousDepositTotal) + parseFloat(newDepositAmount);    // here the values are string. So it need to convert to float. 
     depositTotalElement.innerText = currentDepositTotal;
 
 
-    // step 5: Get the balance of the current. 
+    // Step 5. (Balance Part)
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotal = balanceTotalElement.innerText;
 
-    // step 6: get balance current total. 
+    
     const currentBalanceTotal = parseFloat(previousBalanceTotal) + parseFloat(newDepositAmount);
     balanceTotalElement.innerText = currentBalanceTotal;
 
-    // step : deposit field will be clear. 
+    // step 7. (deposit field will be clear) 
     depositField.value = ''; 
-})
+});
 
--- withdraw.js
+++ withdraw.js
@@ -3,24 +3,35 @@ step 1: add event handler with withdraw button.
 step 2: get the withdraw amount from the withdraw input field. 
 step 3: get the previous withdraw total. 
 step 4: calculate total withdraw amount. 
-step 5: 
+step 5: get the previous balance. 
+step 6: calculate the total balance and then 
+step 7: clear the input field. 
 
 */
 
 // step 1.
 document.getElementById('btn-withdraw').addEventListener('click', function(){
-    console.log('Total');
-    // step 2.
+    // step 2.(Withdraw part)
     const withdrawField = document.getElementById('withdraw-field');
-    const newWithdraw = withdrawField.value;
+    const newWithdrawAmount = withdrawField.value;
 
     // step 3.
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotal = withdrawTotalElement.innerText;
-    console.log(previousWithdrawTotal);
 
     // step 4.
+    const currentWithdrawTotal = parseFloat(previousWithdrawTotal) + parseFloat(newWithdrawAmount);
+    withdrawTotalElement.innerText = currentWithdrawTotal;
 
+    // step 5. (Balance part)
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotalElement.innerText;
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    // step 7. (clear the input field)
+    withdrawField.value = '';
 });
 
 
